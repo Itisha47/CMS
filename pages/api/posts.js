abea9efc79d4bad3b6d44268efadcdfc3d25954d
itishac <itisha.chowdhury@infinite.com>
2024-11-26 10:12:11 +0530

    api logs

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -10,6 +10,7 @@ export default async function handler(req, res) {
       console.log(posts);
       res.status(200).json(posts);
     } catch (error) {
+        console.log(error)
       res.status(500).json({ error: "Failed to fetch posts" });
     }
     
